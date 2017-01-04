200affb7752f60d6dbdd2ad572c00840e18d69d8
Grrw <GitYBenY-a4h1@mailnicks.org>
2017-01-04 15:31:32 -0800

    Finalized Planets and Characters

--- starExplorer.py
+++ starExplorer.py
@@ -18,14 +18,14 @@ while not mExit:
         pSel = 1
         while not cExit:
             print("Loading...")
-            cResponse = requests.get('http://swapi.co/api/people/' + str(cAcc))
-            if pSel == 1:
+            try:
+                cResponse = requests.get('http://swapi.co/api/people/' + str(cAcc), timeout = 10)
                 func.charScreen(cResponse, cAcc)
-            elif pSel == 2:
-                func.charScreen2(cResponse, cAcc)
+            except:
+                func.tryFailed()
             # print(cAcc)
             cSelect = getch.getch()
-            if cSelect == 'q':
+            if cSelect == 'q': # Quit
                 cExit = True
             elif cSelect == 'h':
                 cAcc = cAcc - 1
@@ -39,7 +39,27 @@ while not mExit:
                 # print(cAcc)
     # Planets
     elif mSelect == 'p':
-        pass
+        pExit = False
+        pAcc = 1
+        while not pExit:
+            print("Loading...")
+            try:
+                pResponse = requests.get('http://swapi.co/api/planets/' + str(pAcc), timeout = 10)
+                func.planetScreen(pResponse, pAcc)
+            except:
+                func.tryFailed()
+            pSelect = getch.getch()
+            if pSelect == 'q': # Quit 
+                pExit = True
+            elif pSelect == 'h':
+                pAcc = pAcc - 1
+                if pAcc <= 0:
+                    pAcc = 61
+            elif pSelect == 'l':
+                pAcc = pAcc +1
+                if pAcc >= 62:
+                    pAcc = 1
+
     # Films
     elif mSelect == 'f':
         pass
